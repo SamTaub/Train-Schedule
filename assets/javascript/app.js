a3a351009bcffda6f101f4f4f8554f281a573d09
Sam Taub <staub91@gmail.com>
2018-12-17 15:07:57 -0500

    updated data table and input form

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,12 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyALxjaeJ0Ii3ZdSmiMx3hR94oJ7kVPBOnU",
-    authDomain: "train-schedule-58361.firebaseapp.com",
-    databaseURL: "https://train-schedule-58361.firebaseio.com",
-    projectId: "train-schedule-58361",
-    storageBucket: "train-schedule-58361.appspot.com",
-    messagingSenderId: "669043110510"
-  };
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyALxjaeJ0Ii3ZdSmiMx3hR94oJ7kVPBOnU",
+  authDomain: "train-schedule-58361.firebaseapp.com",
+  databaseURL: "https://train-schedule-58361.firebaseio.com",
+  projectId: "train-schedule-58361",
+  storageBucket: "train-schedule-58361.appspot.com",
+  messagingSenderId: "669043110510"
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  
