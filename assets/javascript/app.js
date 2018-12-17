63a552226e64135ce630fd129acb8c3ba9700aa6
Sam Taub <staub91@gmail.com>
2018-12-17 17:42:24 -0500

    data correctly displays in table

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,17 +26,36 @@ database.ref().on("child_added", function(snapshot){
   var sv = snapshot.val();
 
   var createTableRow = $("<tr>");
-  var createTableData = $("<td>");
 
-  var addTrain = createTableData.text(sv.train);
-  var addDestination = createTableData.text(sv.destination);
-  var addNextArrival = createTableData.text(sv.nextArrival);
-  var addMinutesAway = createTableData.text(sv.minutesAway);
-  var addFrequency = createTableData.text(sv.frequency);
+  var addTrain = $("<td>").text(sv.train);
+  var addDestination =  $("<td>").text(sv.destination);
+  var addNextArrival = $("<td>").text(sv.nextArrival);
+  var addMinutesAway =  $("<td>").text(sv.minutesAway);
+  var addFrequency =  $("<td>").text(sv.frequency);
 
   createTableRow.append(addTrain, addDestination, addNextArrival, addMinutesAway, addFrequency);
 
   $("tbody").append(createTableRow);
 
+});
+
+$("#submit").on("click", function(){
+
+  event.preventDefault();
+
+  train = $("#formTrain").val().trim();
+  destination = $("#formDestination").val().trim();
+  nextArrival = $("#formFirstTrain").val().trim();
+  frequency = $("#formFrequency").val().trim();
+
+
+  database.ref().push({
+    train: train,
+    destination: destination,
+    nextArrival: nextArrival,
+    frequency: frequency,
+    minutesAway: "placeholder"
+
+  });
 
 });
