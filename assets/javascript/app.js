2755bb99690f61479b7e05727f5638b410b7fbce
Sam Taub <staub91@gmail.com>
2018-12-17 13:41:00 -0500

    added firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyALxjaeJ0Ii3ZdSmiMx3hR94oJ7kVPBOnU",
+    authDomain: "train-schedule-58361.firebaseapp.com",
+    databaseURL: "https://train-schedule-58361.firebaseio.com",
+    projectId: "train-schedule-58361",
+    storageBucket: "train-schedule-58361.appspot.com",
+    messagingSenderId: "669043110510"
+  };
+  
+  firebase.initializeApp(config);
